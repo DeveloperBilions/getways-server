e60bd81a529f9038f9ccd8f487c8c32a1c6bb821
ramya-thebilions <ramya@thebilions.com>
2024-12-16 11:29:22 +0530

    Authentication flow

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,7 @@
 async function loadModule() {
     const module = await import('./functions.js');
     // Use the imported module
+    await import('./getways_api/getways_api.js');
 }
 
 loadModule();
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const { ParseServer } = require("parse-server");
 const ParseDashboard = require("parse-dashboard");
 const cors = require("cors");
@@ -8,6 +9,7 @@ const app = express();
 
 // Add CORS middleware
 app.use(cors());
+app.use(express.json());
 
 // Parse Server initialization
 async function startParseServer() {
@@ -56,6 +58,39 @@ async function startParseServer() {
       `##### parse-server running on ${process.env.SERVER_URL} #####`
     );
   });
+
+  //auth flow for AOG API 
+app.post('/requestToken', (req, res) => {
+  const user = { id: req.body.userid}
+
+  const ACCESS_TOKEN_SECRET='e11c6aa82db982f9fa9215e244a43a4f3e436ef4b4576766845976ab526accfed74e21ceb19265f88635c04a0222593eaca3b696ed2be5a38854f1869bfdad8e';
+
+  const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET);
+  res.json({accessToken: accessToken});    
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  const ACCESS_TOKEN_SECRET='e11c6aa82db982f9fa9215e244a43a4f3e436ef4b4576766845976ab526accfed74e21ceb19265f88635c04a0222593eaca3b696ed2be5a38854f1869bfdad8e';
+
+  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+
+}
+
+app.get('/posts', authenticateToken, (req, res) => {
+  const posts = [
+    { userid: "Em0FNBjBHc", title: "Post 1" }
+    , { userid: "Fm0FNBjBHc", title: "Post 2" }
+  ];
+  res.json(posts);
+});
 
   // Cron job to run every 1 minutes
   cron.schedule('*/1 * * * *', async () => {
